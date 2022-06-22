919ba11a7dcaaf2acfd7580f06a5956952ccd0f6
Martin <martwainm@gmail.com>
2022-06-22 22:33:51 +0300

    commit tree 4723de61789514b651f0abdb61ac9476b99e0a62

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  return word.split("").reverse().join("")
+}
+
+function isPalindrome(word){
+  return word == reverseString(word)
 }
 
 /* 
   Add your pseudocode here
+  reverse the input string
+
+  if the reversed string is the same as the input
+    return true
+  else
+    return false  
 */
 
 /*
   Add written explanation of your solution here
+  function reverseString(word) {
+  // create an array from the input string
+  const wordArray = word.split("");
+  // reverse the array
+  const reversedWordArray = wordArray.reverse();
+  // create a string from the reversed array
+  const reversedWord = reversedWordArray.join("");
+  // return the reversed string
+  return reversedWord
+}
+
+function isPalindrome(word) {
+  // reverse the input string by calling reverseString() and assign the return value to a variable reverseWord
+  const reversedWord = reverseString(word);
+  // test if the reversed string is the same as the input
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
+}
 */
 
 // You can run `node index.js` to view these console logs
